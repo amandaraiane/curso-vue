7fbb820e37f02d05387f8b1a2627bbd8ecc7ee55
amandaraiane <amanda127alves@gmail.com>
2022-06-23 09:33:13 -0300

    assunto: introdução a componentes

---
+++ componentes/componente-intro/src/App.vue
@@ -0,0 +1,20 @@
+<template>
+  <div id="app">
+    <h1>Contadores</h1>
+    <app-contador></app-contador>
+  </div>
+</template>
+
+<script>
+  export default{
+    data(){
+      return {
+        contador: 0
+      }
+    }
+  }
+</script>
+
+<style>
+
+</style>
