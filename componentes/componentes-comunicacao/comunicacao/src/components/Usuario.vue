69c9a677df9c972904cca76b41821469d33f201e
amandaraiane <amanda127alves@gmail.com>
2022-06-24 11:04:35 -0300

    assunto: comunicação entre componentes

---
+++ componentes/componentes-comunicacao/comunicacao/src/components/Usuario.vue
@@ -0,0 +1,57 @@
+<template>
+    <div class="container">
+        <h1>Componente Usuário</h1>
+        <p>Esse é um componente muito legal!</p>
+        <p>Nome é: <strong>{{ nome }}</strong></p>
+        <p>Idade: <span>{{ idade }}</span></p>
+        <button @click="alterarNome">Alterar nome</button>
+        <hr>
+        <div class="componentes">
+            <app-usuario-info :nome="nome" @nomeMudou="nome = $event" :reiniciarFn="reiniciarNome" :idade="idade"/>
+            <app-usuario-editar :idade="idade" @idadeMudou="idade = $event"/>
+        </div>
+    </div>
+</template>
+
+<script>
+import AppUsuarioInfo from './UsuarioInfo'
+import AppUsuarioEditar from './UsuarioEditar'
+
+export default {
+    components: { AppUsuarioInfo, AppUsuarioEditar },
+    data() {
+        return {
+            nome: 'Maria',
+            idade: 21
+        }
+    },
+    methods: {
+        alterarNome(){
+            this.nome = 'Ana'
+        },
+        reiniciarNome(){
+            this.nome = 'Maria'
+        }
+    }
+}
+</script>
+
+<style scoped>
+    .container {
+        background-color: #27363b;
+        color: #fff;
+        padding: 10px;
+    }
+
+    .container hr {
+        margin: 20px 10px;
+    }
+
+    .componentes {
+        display: flex;
+    }
+
+    .componentes > * {
+        margin: 10px;
+    }
+</style>
---
+++ componentes/componentes-comunicacao/comunicacao/src/components/UsuarioInfo.vue
@@ -0,0 +1,45 @@
+<template>
+    <div class="componente">
+        <h2>As Informações de Usuário</h2>
+        <p>Vários detalhes...</p>
+        <p>Nome do usuário <strong>{{ nome }}</strong></p>
+        <p>Nome do usuário invertido <strong>{{ inverterNome() }}</strong></p>
+        <p>Idade do Usuário: <strong>{{ idade }}</strong></p>
+        <button @click="reiniciarNome">Reiniciar nome</button>
+        <button @click="reiniciarFn">Reiniciar nome (Callback)</button>
+    </div>
+</template>
+
+<script>
+export default {
+    //props: ['nome'],
+    props: {
+        nome: {
+            type: String,
+            required: true,
+            default: 'Anonimo'
+        },
+        reiniciarFn: Function,
+        idade: Number
+    },
+
+    methods: {
+        inverterNome(){
+            return this.nome.split('').reverse().join('')
+        },
+        reiniciarNome(){
+            this.nome = "Maria"
+            this.$emit('nomeMudou', this.nome)
+        }
+    }
+                
+}
+</script>
+
+<style scoped>
+    .componente {
+        flex: 1;
+        background-color: #ec485f;
+        color: #fff;
+    }
+</style>
