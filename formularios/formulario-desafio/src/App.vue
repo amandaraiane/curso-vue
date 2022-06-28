9698486f8b6b48499d3cbcc1141b035aa3a58662
amandaraiane <amanda127alves@gmail.com>
2022-06-28 13:43:18 -0300

    assunto: formularios

---
+++ formularios/formulario-desafio/src/App.vue
@@ -0,0 +1,151 @@
+<template>
+	<div id="app">
+		<h1>Formulário Desafio</h1>
+		<div class="conteudo">
+			<form class="painel" v-if="exibir">
+				<div class="cabecalho">Formulário</div>
+				<Rotulo nome="Nome">
+					<input type="text" v-model="usuario.nome"/>
+				</Rotulo>
+				<Rotulo nome="Sobrenome">
+					<input type="text" v-model="usuario.sobrenome"/>
+				</Rotulo>
+				<Rotulo nome="E-mail">
+					<input type="email" v-model="usuario.email"/>
+				</Rotulo>
+				<Rotulo nome="Senha">
+					<input type="password" v-model="usuario.senha"/>
+				</Rotulo>
+				<Rotulo nome="Armazenar Dados?">
+					<span class="mr-4"><input type="radio" value="Sim" v-model="armazenamento"/>Sim</span>
+					<span class="mr-4"><input type="radio" value="Não" v-model="armazenamento"/>Não</span>
+				</Rotulo>
+				<hr/>
+				<button @click.prevent="enviar">Enviar</button>
+				
+			</form>
+			<div class="painel" v-else>
+				<div class="cabecalho">Resultado</div>
+				<Rotulo nome="Nome">
+					<span>{{ usuario.nome }}</span>
+				</Rotulo>
+				<Rotulo nome="Sobrenome">
+					<span>{{ usuario.sobrenome }}</span>
+				</Rotulo>
+				<Rotulo nome="E-mail">
+					<span>{{ usuario.email }}</span>
+				</Rotulo>
+				<Rotulo nome="Senha">
+					<span>{{ usuario.senha }}</span>
+				</Rotulo>
+				<Rotulo nome="Armazernar Dados:">
+					<span>{{ armazenamento }}</span>
+				</Rotulo>
+
+			</div>
+		</div>
+	</div>
+<!-- Exercicio 01 -->
+<!-- Criar uma formulário de registro -->
+<!-- Nome completo (Nome e Sobrenome) -->
+<!-- Email -->
+<!-- Senha -->
+<!-- Armazenar Dados? (Sim/Não) -->
+
+<!-- Exercicio 02 -->
+<!-- Só mostrar o fomulário de não tiver sido submetido -->
+<!-- Mostrar a área de Resultado apenas quando o formulário for submetido -->
+
+<!-- Exercicio 03 -->
+<!-- Crie um componente personalizado NomeCompleto -->
+<!-- Esse componente deve receber Nome e Sobrenome -->
+</template>
+
+<script>
+import Rotulo from './components/Rotulo.vue'
+
+export default {
+	name: 'app',
+	components: { Rotulo },
+	methods: {
+		enviar() {
+			this.exibir = false
+		}
+	},
+	data() {
+		return {
+			armazenamento: 'Sim',
+			usuario: {
+				nome: '',
+				sobrenome: '',
+				email: '',
+				senha: '',
+
+			},
+			exibir: true
+		}
+	}
+}
+</script>
+
+<style>
+
+body {
+	background-color: #ECECEC;
+}
+
+#app {
+	font-family: 'Avenir', Helvetica, Arial, sans-serif;
+	-webkit-font-smoothing: antialiased;
+	-moz-osx-font-smoothing: grayscale;
+	text-align: center;
+	color: #2c3e50;
+
+	display: flex;
+	flex-direction: column;
+}
+
+.conteudo {
+	display: flex;
+}
+
+.painel {
+	flex: 1;
+	background: #FFF;
+	margin: 0px 10px;
+	padding: 20px;
+	border: 1px solid #AAA;
+	border-radius: 5px;
+}
+
+.painel .cabecalho {
+	width: 100%;
+	background-color: #DDD;
+	padding: 10px 0px;
+	border-radius: 5px;
+	font-size: 1.4rem;
+}
+
+#app form button {
+	float: right;
+	margin: 10px 0px;
+	padding: 10px 20px;
+	font-size: 1.4rem;
+	border-radius: 5px;
+	color: #FFF;
+	background-color: #2196F3;
+}
+#app form button:hover{
+	cursor: pointer;	
+}
+
+#app h1 {
+	font-weight: 200;
+	margin: 20px;
+	padding: 0;
+}
+
+.mr-4 {
+	margin-right: 40px;
+}
+</style>
