7f5f4635e84268e9cd494e01076bd74b10b176ef
amandaraiane <amanda127alves@gmail.com>
2022-06-28 16:31:07 -0300

    assunto: filtros

---
+++ filtros-mixins/filtros-mixins-exercicios/src/App.vue
@@ -0,0 +1,44 @@
+<template>
+	<div id="app">
+		<h1>Filtros & Mixins</h1>
+		<hr/>
+		<p>{{ cpf | cpf }}</p> <!-- após o | passamos o nome do filtro, nesse caso 'cpf'-->
+		<p>{{ cpf | cpf | inverter }}</p> <!-- com filtro global 'inverter'-->
+		<input type="text" :value="cpf | cpf"/> <!-- passando filtro dentro de um v-bind-->
+	</div>
+</template>
+
+<script>
+export default {
+	filters: {//definindo filtros de maneira local
+		cpf(valor){
+			const arr = valor.split('')
+			arr.splice(3, 0, '.')
+			arr.splice(7, 0, '.')
+			arr.splice(11, 0, '-')
+			return arr.join('')
+		}
+	},
+	data() {
+		return {
+			cpf: '00286005589'
+		}
+	}
+}
+</script>
+
+<style>
+#app {
+	font-family: 'Avenir', Helvetica, Arial, sans-serif;
+	-webkit-font-smoothing: antialiased;
+	-moz-osx-font-smoothing: grayscale;
+	text-align: center;
+	color: #2c3e50;
+	margin-top: 60px;
+	font-size: 2.5rem;
+}
+
+input {
+	font-size: 2.5rem;
+}
+</style>
